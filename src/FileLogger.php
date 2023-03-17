6dfc3bf909bef513b8f5c0e36b2cf0f0c5bed680
Noah Eberharter <neberharter@schueler.hakzell.at>
2023-03-17 09:24:15 +0100

    create file logger

--- src/FileLogger.php
+++ src/FileLogger.php
@@ -1,1 +1,22 @@
 <?php
+namespace Neberharter\Logger;
+
+class FileLogger implements ILogger
+{
+    private $handle = null;
+
+    public function __construct(string $filename)
+    {
+        $this->handle = fopen($filename, 'a');
+    }
+
+    public function LogEntry(string $line): void
+    {
+        fwrite($this->handle, $line . "\n");
+    }
+
+    public function __destruct()
+    {
+        fclose($this->handle);
+    }
+}
